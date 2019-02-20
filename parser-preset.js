9ae723ee29317b40ae7e436e8bf4740062710a57
段佳思 <jsduan@duanjiasideMacBook-Pro.local>
2019-02-20 21:26:50 +0800

    docs: some message

---
+++ parser-preset.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parserOpts: {
+    headerPattern: /^(\w*)\((\w*)\)-(\w*)\s(.*)$/,
+    headerCorrespondence: [
+      'type',
+      'scope',
+      'ticket',
+      'subject'
+    ]
+  }
+};
